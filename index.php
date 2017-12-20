c805e36217ec3119675315d340163308e5d4393a
Vladimir Prokhnenko <volmir@ukr.net>
2017-12-20 12:03:15 +0200

    Add project files

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 use PVV\Application;
  
 $loader = require( __DIR__ . '/vendor/autoload.php' );
-$loader->addPsr4( 'PVV\\', __DIR__ . '/src/' );
 
 $appication = new Application();
 $appication->run();
